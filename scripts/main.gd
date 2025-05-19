7ce78c8d9495200f2336ab5f0ed64f5bdf793648
Doejd <dzlatev56@gmail.com>
2025-05-19 14:44:04 +0300

    Fixed a dumb error

--- scripts/main.gd
+++ scripts/main.gd
@@ -41,7 +41,7 @@ func _input(_event: InputEvent) -> void:
 		is_open_file_picker = false
 	elif Input.is_action_just_pressed("ui_up") and cur_ind > 0:
 		cur_ind -= 1
-	elif Input.is_action_just_pressed("ui_down") and cur_ind < item_list.get_item_count():
+	elif Input.is_action_just_pressed("ui_down") and cur_ind < item_list.get_item_count()-1:
 		cur_ind += 1
 	elif Input.is_action_just_pressed("save"):
 		var full_path = dir.get_current_dir().path_join(item_list.get_item_text(cur_ind))
