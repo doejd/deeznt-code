37b56cbb071e7bfb1afc61e24a390bdb05c4e56f
Dorian Zlatev <dzlatev56@gmail.com>
2025-05-24 22:18:58 +0300

    Theme support is here!!!

--- scripts/edit.gd
+++ scripts/edit.gd
@@ -15,6 +15,16 @@ var keywords: Dictionary = {
 	"function":   str_to_clr("82aaff"),
 	"member":     str_to_clr("c792ea")
 }
+var GUI : Dictionary = {
+	"background_color": str_to_clr("23272e"),
+	"current_line_color": str_to_clr("2c313c"),
+	"selection_color": str_to_clr("3d4556"),
+	"font_color": str_to_clr("e06c75"),
+	"word_highlighted_color": str_to_clr("3d4556"),
+	"completion_background_color": str_to_clr("1e2227"),
+	"completion_selected_color": str_to_clr("2c313a"),
+	"caret_color": str_to_clr("528bff")
+}
 
 func str_to_clr(string: String) -> Color:
 	return Color.from_string(string, "#ff0000")
@@ -39,7 +49,7 @@ func set_up_extensions(extension : String):
 	lua.bind_libraries(["base", "table", "string"])
 	lua.push_variant("highlight", highlight)
 	lua.push_variant("highlight_region", highlight_region)
-	var error = lua.do_file("res://langs/" + extension + ".lua")
+	var error = lua.do_file("res://Lua/langs/" + extension + ".lua")
 	if error is LuaError:
 		print("ERROR %d: %s" % [error.type, error.message])
 		return
@@ -58,3 +68,24 @@ func setup_highlighter() -> void:
 	for entry in crth:
 		if CH.has_color_region(entry[0]): continue
 		CH.add_color_region(entry[0], entry[1], keywords[entry[2]], entry[3])
+
+func set_keywords(keyword : String, color : String):
+	keywords[keyword] = str_to_clr(color)
+	
+func set_gui(property : String, new_color : String):
+	if not (property in GUI.keys()):
+		print("ERROR: provided color property (\"%s\") in theme (GUI) is invalid." % [property])
+		return
+		
+	GUI[property] = str_to_clr(new_color)
+
+func setup_theme():
+	lua.push_variant("set_keywords", set_keywords)
+	lua.push_variant("set_gui", set_gui)
+	var error = lua.do_file("res://Lua/themes/One Dark Pro Darker.lua")
+	if error is LuaError:
+		print("ERROR %d: %s" % [error.type, error.message])
+		return
+
+func _ready() -> void:
+	setup_theme()
