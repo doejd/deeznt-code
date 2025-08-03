b22ec47f2cf26127110d11b3b1c1fc4f0c74d4ae
Doejd <dzlatev56@gmail.com>
2025-08-03 11:33:26 +0300

    Add files via upload
    Compiled Linux pseudoterminal

---
+++ main.h
@@ -0,0 +1,36 @@
+#ifndef CMD_HOST_H
+#define CMD_HOST_H
+
+#include <godot_cpp/classes/text_edit.hpp>
+#include <godot_cpp/core/class_db.hpp>
+#include <thread>
+#include <atomic>
+
+using namespace godot;
+
+class CmdHost : public TextEdit {
+    GDCLASS(CmdHost, TextEdit);
+
+private:
+    int master_fd = -1;
+    pid_t child_pid = -1;
+    std::thread reader_thread;
+    std::atomic<bool> running = false;
+    
+protected:
+    static void _bind_methods();
+
+public:
+    CmdHost();
+    ~CmdHost();
+    virtual void _ready();
+    virtual void _exit_tree();
+    void main_loop();
+    void start_pseudoterminal_session();
+    void end_pseudoterminal_session();
+    void _gui_input(const Ref<InputEvent> &event);
+    void edit_text(const String &newtext);
+    void write_to_terminal(const String &input);
+};
+
+#endif // CMD_HOST_H
